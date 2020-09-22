904ab2ffc835fee149b1556bc529a2cecea05d90
JhonMurilloH <jhon-murillo@hotmail.com>
2020-09-22 16:41:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zfnwja
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zfnwja)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'jhonH',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Wordl!!!';
+  dueno = 'Jhoncillo'
+}
